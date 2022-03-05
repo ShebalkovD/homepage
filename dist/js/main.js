4922283a760d637e6691dc735883af63b3c0453f
ShebalkovD <shebalkovdenis.wk@gmail.com>
2022-03-05 21:26:46 +0300

    -

---
+++ dist/js/main.js
@@ -0,0 +1,12 @@
+const burger = document.querySelector('.burger')
+const burgerMenu = document.querySelector('.burger__menu')
+burger.onclick = () => {
+    burgerMenu.classList.toggle('burger__active')
+}
+
+addEventListener('click', () => {
+    if( event.target != burger  ){
+      burgerMenu.classList.remove('burger__active') 
+    }
+})
+
