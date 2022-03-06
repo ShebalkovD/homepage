6fdc97af7034425084c32fd59d7241d9e71a78ae
ShebalkovD <shebalkovdenis.wk@gmail.com>
2022-03-06 11:51:23 +0300

    burger fix

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,12 +1,41 @@
-const burger = document.querySelector('.burger')
-const burgerMenu = document.querySelector('.burger__menu')
+let burger = document.querySelector('.burger')
+let burgerMenu = document.querySelector('.burger__menu')
+let burgerItem = document.querySelectorAll('.burger__menu-item')
+let body = document.querySelector('body')
 burger.onclick = () => {
-  burgerMenu.classList.toggle('burger__active')
+ 
+  burgerMenu.classList.add('burger__active')
+ 
+  burgerMenu.classList.toggle('animate__fadeInRight')
+  burger.classList.toggle('burgerBtn__active')
+  body.classList.toggle('lock')
+  if(burgerMenu.classList.contains('burger__active')){
+    burgerMenu.classList.toggle('animate__fadeOutRight')
+
+    // setTimeout(() => burgerMenu.classList.remove('burger__active'), 500)
+  }
 }
 
-addEventListener('click', () => {
-    if( event.target != burger  ){
-      burgerMenu.classList.remove('burger__active') 
-    }
+burgerItem.forEach((el) => {
+  el.onclick = () => {
+    burgerMenu.classList.remove('burger__active')
+    burger.classList.remove('burgerBtn__active')
+    body.classList.remove('lock')
+    burgerMenu.classList.add('animate__fadeOutRight')
+    burgerMenu.classList.remove('animate__fadeInRight')
+  }
 })
+
+
+
+
+
+// burger.onclick = () => {
+//   burgerMenu.classList.toggle('burger__active')
+//   burger.classList.toggle('burgerBtn__active')
+//   body.classList.toggle('lock')
+
+// }
+
+
 
